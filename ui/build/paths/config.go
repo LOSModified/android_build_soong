8ded21f4c42e2ac142012378c76c9cf7967a74aa
Sam Mortimer <sam@mortimer.me.uk>
2019-09-04 22:52:59 -0700

    soong: Whitelist nproc
    Change-Id: Ie83688c900a241728b1153cc8ac2aeccc2cb31f6

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -116,6 +116,7 @@ var Configuration = map[string]PathConfig{
 	"locale":         Allowed,
 	"lsof":           Allowed,
 	"m4":             Allowed,
+	"nproc":          Allowed,
 	"openssl":        Allowed,
 	"perl":           Allowed,
 	"prodcertstatus": Allowed,
