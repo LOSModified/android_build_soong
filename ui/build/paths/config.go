10c450a93e540a8edee0791fba7e5bea37fa4e57
hmtheboy154 <buingoc67@gmail.com>
2023-08-13 21:03:30 +0200

    soong: Whitelist pahole
    Change-Id: Id945672e5b3eaacfe0605cfc7d8c372a0d6c76bb

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -122,6 +122,7 @@ var Configuration = map[string]PathConfig{
 	"nproc":          Allowed,
 	"numfmt":         Allowed,
 	"openssl":        Allowed,
+	"pahole":         Allowed,
 	"perl":           Allowed,
 	"pstree":         Allowed,
 	"repo":           Allowed,
