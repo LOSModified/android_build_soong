4e52e1b860c84e0c221c6cabead48a34834ef3e4
johnmart19 <johnivan19999@gmail.com>
2023-09-20 22:24:32 +0800

    soong: Whitelist llvm-nm

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -115,6 +115,7 @@ var Configuration = map[string]PathConfig{
 	"jq":             Allowed,
 	"ld.lld":         Allowed,
 	"llvm-ar":        Allowed,
+	"llvm-nm":        Allowed,
 	"lsof":           Allowed,
 	"m4":             Allowed,
 	"openssl":        Allowed,
