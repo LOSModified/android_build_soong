6fc65a6887275aa936b978969b004cb0224f1e58
Kevin F. Haggerty <haggertk@lineageos.org>
2023-09-20 22:42:10 +0800

    soong: Whitelist zip2zip
    * Needed for fastboot zip generation

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -121,6 +121,7 @@ var Configuration = map[string]PathConfig{
 	"xz":      Allowed,
 	"zcat":    Allowed,
 	"zip":     Allowed,
+	"zip2zip": Allowed,
 
 	"python3.6":  Allowed,
 	"python3.8":  Allowed,
