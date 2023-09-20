9d4c7e0816b398f207c75cb969e92940636e81ac
sbwml <984419930@qq.com>
2023-09-20 23:06:57 +0800

    soong: Allow more toolchains

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -90,6 +90,8 @@ var Configuration = map[string]PathConfig{
 	"bison":          Allowed,
 	"brotli":         Allowed,
 	"ccache":         Allowed,
+	"clang-13":       Allowed,
+	"clang-14":       Allowed,
 	"cpio":           Allowed,
 	"curl":           Allowed,
 	"date":           Allowed,
@@ -198,6 +200,18 @@ var Configuration = map[string]PathConfig{
 	"arm-linux-androideabi-size":         Allowed,
 	"arm-linux-androideabi-strings":      Allowed,
 	"arm-linux-androideabi-strip":        Allowed,
+	"arm-linux-androidkernel-ar":         Allowed,
+	"arm-linux-androidkernel-as":         Allowed,
+	"arm-linux-androidkernel-cpp":        Allowed,
+	"arm-linux-androidkernel-elfedit":    Allowed,
+	"arm-linux-androidkernel-gcc":        Allowed,
+	"arm-linux-androidkernel-ld":         Allowed,
+	"arm-linux-androidkernel-nm":         Allowed,
+	"arm-linux-androidkernel-objcopy":    Allowed,
+	"arm-linux-androidkernel-objdump":    Allowed,
+	"arm-linux-androidkernel-readelf":    Allowed,
+	"arm-linux-androidkernel-size":       Allowed,
+	"arm-linux-androidkernel-strip":      Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
