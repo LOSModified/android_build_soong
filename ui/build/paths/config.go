adc2e636cb1db57935fdea5d8c539abbfb4e86f4
someone5678 <someone5678@users.noreply.github.com>
2024-02-28 09:34:31 +0800

    soong: Whitelist "["
    Log:
    Disallowed PATH tool "[" used: []string{"[", "-f", "../.git/shallow", "]"}
    
    Change-Id: I3ee37215db1ad326fed581b8c13f4a9fd4a174d4

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -144,6 +144,7 @@ var Configuration = map[string]PathConfig{
 	"zip":            Allowed,
 	"zip2zip":        Allowed,
 	"zipdetails":     Allowed,
+	"[":              Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
