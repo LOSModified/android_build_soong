9cd7105e3679ef40858beba25007d8998f5b8d29
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-24 00:09:28 +0800

    soong: Whitelist jq
    Change-Id: I885eca97ed6d625edf411126ac4e81a5a6b4da6b

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -97,6 +97,7 @@ var Configuration = map[string]PathConfig{
 	"jar":     Allowed,
 	"java":    Allowed,
 	"javap":   Allowed,
+	"jq":      Allowed,
 	"lsof":    Allowed,
 	"m4":      Allowed,
 	"openssl": Allowed,
